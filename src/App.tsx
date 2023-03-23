92881201e781eaa14b7e22dbc5c4ebfe0480bfc5
cws-ctrl <CWS_CTRL@163.com>
2023-03-24 01:25:29 +0800

    feat：deploy netlify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <p className="text-red-500 text-3xl">react</p>
+  return <p className="text-red-500 text-3xl">cws-draw</p>
 };
 
 export default App;
