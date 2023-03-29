cf975d157a1e2f4acab51503117ee9c171563450
cws-ctrl <CWS_CTRL@163.com>
2023-03-30 04:55:43 +0800

    style：remonder

--- src/views/exhibition/cpns/reminder.tsx
+++ src/views/exhibition/cpns/reminder.tsx
@@ -4,18 +4,13 @@ import githubSVG from "../../../assets/image/github.svg"
 
 const Reminder = () => {
     return <div className="relative h-10 mt-5 mb-10">
-  <img src={logoSVG} alt="logo" className="absolute top-3 left-10 sm:left-1/6 w-44" />
+  <img src={logoSVG} alt="logo" className="absolute top-3 left-8 sm:left-32 w-48" />
   <a href="https://github.com/CWS-CTRL/svgloading" target="_blank" rel="noopener noreferrer">
-    <img src={githubSVG} alt="github" className="absolute top-4 right-10 sm:right-1/6 w-12" />
+    <img src={githubSVG} alt="github" className="absolute top-5 right-8 sm:right-32  w-12" />
   </a>
 
 </div>
     
-    // return <div className="grid grid-cols-2 mt-5 mb-10">
-    //     <div className="ml-10"><img src={logoSVG} alt="logo" className="w-40" /></div>
-    //     <div className="grid justify-center align-middle"><img src={ githubSVG} alt="github" className="w-10" /></div>
-    //     <div></div>
-    // </div>
 }
 
 export default Reminder
