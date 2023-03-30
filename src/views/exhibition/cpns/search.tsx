f2cf0fe4b621a677c3707b4d3ecb9c3e312e7453
cws-ctrl <CWS_CTRL@163.com>
2023-03-30 11:52:13 +0800

    style：search

--- src/views/exhibition/cpns/search.tsx
+++ src/views/exhibition/cpns/search.tsx
@@ -8,7 +8,7 @@ const Search = () => {
 
     return <div className="grid content-center w-5/6 md:w-1/2 h-24 mx-auto mt-10 mb-8">
         <div className="relative w-5/6 mx-auto">
-            <input type="text" className="w-full h-12 px-8 rounded-2xl text-neutral-700 font-semibold" ref={ inputRef} />
+            <input type="text" placeholder="Not yet realized" className="w-full h-12 pl-8 pr-12 sm:pr-16  rounded-2xl text-neutral-700 font-semibold" ref={ inputRef} />
             <img src={searchSVG} alt="search" className="absolute top-3 sm:top-1 right-5 w-6 sm:w-10 aspect-square cursor-pointer"/>
         </div>
     </div>
