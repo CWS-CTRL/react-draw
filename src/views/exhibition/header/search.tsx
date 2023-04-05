5527e1658d24280c8ccd3bc51659593df6deb935
cws-ctrl <CWS_CTRL@163.com>
2023-04-05 19:06:07 +0800

    chore：modify seach placeholder

--- src/views/exhibition/header/search.tsx
+++ src/views/exhibition/header/search.tsx
@@ -26,7 +26,7 @@ const Search = () => {
 
     return <div className="grid content-center w-5/6 md:w-1/2 h-24 mx-auto mt-10 mb-8">
         <div className="relative w-5/6 mx-auto">
-            <input type="text" value={searchKeyWord} placeholder="Not yet realized" onChange={(e) => { action(changeSearchKeyWords(e.target.value)) }} className="w-full h-12 pl-8 pr-12 sm:pr-16  rounded-2xl text-neutral-700 font-semibold" ref={inputRef} onKeyDown={(e) => e.key === "Enter" && navigateLoading()} />
+            <input type="text" value={searchKeyWord} placeholder="try to input：p|l|r|c|m" onChange={(e) => { action(changeSearchKeyWords(e.target.value)) }} className="w-full h-12 pl-8 pr-12 sm:pr-16  rounded-2xl text-neutral-700 font-semibold" ref={inputRef} onKeyDown={(e) => e.key === "Enter" && navigateLoading()} />
             <img src={searchSVG} alt="search" onClick={navigateLoading} className="absolute top-3 sm:top-1 right-5 w-6 sm:w-10 aspect-square cursor-pointer" />
         </div>
     </div>
