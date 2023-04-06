b7b087ca7c14f2b4e01a30250a98a3410ae4d243
cws-ctrl <CWS_CTRL@163.com>
2023-04-06 16:57:39 +0800

    fix：strokeWidth

--- src/views/svg/loading/all/m/mm00.tsx
+++ src/views/svg/loading/all/m/mm00.tsx
@@ -20,7 +20,7 @@ const MM00 = ({ options = {} }: SVGProps) => {
                 <animate href={getExclusiveId("#p2")} attributeType="XML" attributeName="stroke-dashoffset" values="70;70;0"
                     keyTimes="0;0.084;1" dur={22 * dur / 10} repeatCount="1" />
             </g>
-            <g id={getExclusiveId("c")} fill={color} strokeWidth={1 * stroke / 10}>
+            <g id={getExclusiveId("c")} fill={color} strokeWidth={stroke}>
                 <circle id={getExclusiveId("c1")} cx="50" cy="58" r="1" opacity="0" />
                 <circle id={getExclusiveId("c2")} cx="45" cy="58" r="1" opacity="0" />
                 <circle id={getExclusiveId("c3")} cx="55" cy="58" r="1" opacity="0" />
